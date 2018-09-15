6335590ce4d339f740c841bfefa691828133610b
Hideyuki Hirauchi <hirauchi@ideapump.net>
2018-09-15 14:32:39 +0900

    fix #2, add mutex

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const logger = require('gorilog')('ezdbee')
 const fs = require('fs-extra')
+const Mutex = require('await-semaphore').Mutex
 
 class FileDriver {
 
@@ -27,6 +28,7 @@ class EZDBee {
     this.opt = opt
     this.driver = new FileDriver(this.opt)
     this.json = null
+    this.mutex = new Mutex()
     logger.trace('constructor:', this.opt, this.driver)
   }
 
@@ -55,36 +57,40 @@ class EZDBee {
   }
 
   async read(fn) {
-    const org = JSON.stringify(this.json)
-    const _json = fn(JSON.parse(org))
-    if (_json) {
-      logger.warn('This is read only method. Not modified.', this.opt)
-    } else {
-      logger.debug('read only ok.', this.opt)
-    }
+    this.mutex.use(async ()=>{
+      const org = JSON.stringify(this.json)
+      const _json = fn(JSON.parse(org))
+      if (_json) {
+        logger.warn('This is read only method. Not modified.', this.opt)
+      } else {
+        logger.debug('read only ok.', this.opt)
+      }
+    })
   }
 
   async transaction(fn) {
-    const org = JSON.stringify(this.json)
-    const _json = fn(JSON.parse(org))
-    if (! _json) {
-      logger.debug('not modified ok1.', this.opt)
-      return
-    }
+    this.mutex.use(async ()=>{
+      const org = JSON.stringify(this.json)
+      const _json = fn(JSON.parse(org))
+      if (! _json) {
+        logger.debug('not modified ok1.', this.opt)
+        return
+      }
 
-    const mod = JSON.stringify(_json)
-    if (mod == org) {
-      logger.debug('not modified ok2.', this.opt)
-      return
-    }
+      const mod = JSON.stringify(_json)
+      if (mod == org) {
+        logger.debug('not modified ok2.', this.opt)
+        return
+      }
 
-    try {
-      this.json = _json
-      await this.driver.save(this.json)
-      logger.debug('save ok.', this.opt)
-    } catch (err) {
-      logger.error('save err.', this.opt, err)
-    }
+      try {
+        this.json = _json
+        await this.driver.save(this.json)
+        logger.debug('save ok.', this.opt)
+      } catch (err) {
+        logger.error('save err.', this.opt, err)
+      }
+    })
   }
 
 }
